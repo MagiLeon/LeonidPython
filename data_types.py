d7b550f6341d113bd071908a76bcbff9f11413c2
Леонид <kh0rmisha024@gmail.com>
2022-11-17 11:03:19 +0300

    my first2 homework

---
+++ data_types.py
@@ -0,0 +1,11 @@
+int1 = 1
+int23 = 224
+ultra = 70
+some_zminna = ("Hello World!")
+zminna = ("Я вчу пайтон")
+zmin = 0569059
+akbar = ("Ukrain")
+urara = ("Uzhe Vosmaya")
+devyataja = (mgkgkrhkhkrhd)
+uraposlednaja = ("Eto poslednjaja")
+print(int23)
